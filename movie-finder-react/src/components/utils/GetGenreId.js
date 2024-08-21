87561463c7daf82cc8d55d705184960fcaa35b03
kaya70875 <kaya70875@gmail.com>
2024-08-21 22:28:23 +0300

    Updates

--- movie-finder-react/src/components/utils/GetGenreId.js
+++
@@ -1,7 +0,0 @@
-import useFetch from '../../hooks/useFetch'
-
-export default function GetGenreId() {
-    const getGenres = useFetch('/genre/movie/list?language=en&');
-    
-    return getGenres?.genres || [];
-}
