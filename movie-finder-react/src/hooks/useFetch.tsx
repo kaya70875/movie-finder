a96eb44b69a3881e7158679be33e5ebae95411f3
kaya70875 <shortsforturkey@gmail.com>
2024-09-19 19:08:18 +0300

    fix useEffect hook

--- movie-finder-react/src/hooks/useFetch.tsx
+++ movie-finder-react/src/hooks/useFetch.tsx
@@ -1,5 +1,8 @@
 import { useState, useEffect } from 'react';
 
+const BASE_URL = 'https://api.themoviedb.org/3';
+const API_KEY = import.meta.env.VITE_MOVIE_DATABASE_API;
+
 const useFetch = <T,>(url: string) => {
   const [data, setData] = useState<T | null>(null);
   const [loading, setLoading] = useState(true);
@@ -8,7 +11,8 @@ const useFetch = <T,>(url: string) => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch(url);
+        const response = await fetch(`${BASE_URL}${url}&api_key=${API_KEY}`);
+        console.log('resp' , response);
         if (!response.ok) throw new Error('Network response was not ok');
         const result: T = await response.json();
         setData(result);
