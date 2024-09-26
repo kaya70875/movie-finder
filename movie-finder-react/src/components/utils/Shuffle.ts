4ff1af162e91f5f9a27456ab26a8976d5fa51fc8
kaya70875 <shortsforturkey@gmail.com>
2024-09-26 14:49:27 +0300

    update Shuffle.ts

--- movie-finder-react/src/components/utils/Shuffle.ts
+++ movie-finder-react/src/components/utils/Shuffle.ts
@@ -1,4 +1,6 @@
-export function shuffleArray(array : string[] | number[]) {
+import { Movie } from "../../types";
+
+export function shuffleMovies(array : Movie[]) : Movie[] {
     for (let i = array.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
         [array[i], array[j]] = [array[j], array[i]];
