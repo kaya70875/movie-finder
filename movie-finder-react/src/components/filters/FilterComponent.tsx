e7c6ec77842abcffd2d2c98254cfb5b453c84dbe
kaya70875 <shortsforturkey@gmail.com>
2024-10-14 15:25:05 +0300

    update Favorites section to fully resposive

--- movie-finder-react/src/components/filters/FilterComponent.tsx
+++ movie-finder-react/src/components/filters/FilterComponent.tsx
@@ -18,36 +18,39 @@ export default function FilterComponent() {
 
   return (
     <div className="all-dropdowns">
-      <DropdownFilter
-        label={
-          <div className="discover-icons" onClick={() => handleIcons("genre")}>
-            <p>Genre</p>
-            <ChevronIcon isOpen={isFilterOpen.genre} />
-          </div>
-        }
-        items={genres.map((genre) => ({ label: genre.name, value: genre.id }))}
-        onSelect={handleSelectedGenres}
-        selectedItems={sortState.selectedGenres}
-      />
+      <div className="dropdown-filters">
+        <DropdownFilter
+          label={
+            <div className="discover-icons" onClick={() => handleIcons("genre")}>
+              <p>Genre</p>
+              <ChevronIcon isOpen={isFilterOpen.genre} />
+            </div>
+          }
+          items={genres.map((genre) => ({ label: genre.name, value: genre.id }))}
+          onSelect={handleSelectedGenres}
+          selectedItems={sortState.selectedGenres}
+        />
 
-      <DropdownFilter
-        label={
-          <div className="discover-icons" onClick={() => handleIcons("year")}>
-            <p>Release Date</p>
-            <ChevronIcon isOpen={isFilterOpen.year} />
-          </div>
-        }
-        items={years.map((year) => ({ label: year, value: year }))}
-        onSelect={handleSelectedYear}
-        selectedItems={[sortState.selectedYear]}
-      />
+        <DropdownFilter
+          label={
+            <div className="discover-icons" onClick={() => handleIcons("year")}>
+              <p>Release Date</p>
+              <ChevronIcon isOpen={isFilterOpen.year} />
+            </div>
+          }
+          items={years.map((year) => ({ label: year, value: year }))}
+          onSelect={handleSelectedYear}
+          selectedItems={[sortState.selectedYear]}
+        />
+      </div>
+
 
       <div></div>
 
       <SortBySection
-      items={sortOptions}
-      onSelect={handleSelectedSortBy}
-      selectedItems={sortState.selectedSortBy}
+        items={sortOptions}
+        onSelect={handleSelectedSortBy}
+        selectedItems={sortState.selectedSortBy}
 
       />
     </div>
