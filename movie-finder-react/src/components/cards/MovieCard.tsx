f271fdf4283618e100db07752ab0508f01f7b9e5
kaya70875 <shortsforturkey@gmail.com>
2024-10-14 13:17:01 +0300

    add data type to next icons

--- movie-finder-react/src/components/cards/MovieCard.tsx
+++ movie-finder-react/src/components/cards/MovieCard.tsx
@@ -47,7 +47,7 @@ const MovieCard : FC<MovieCardProps> = ({ movies, title , content , mainStyle , 
                     {showFilters && <FilterComponent />}
                 </div>
                 {showScrollButtons &&
-                <div className="next__icons">
+                <div className="next__icons" data-type="movie">
                     <button className='ellipse-button ellipse-button--main' onClick={() => scrollContainer('left')} disabled={scrollPosition === 0 ? true : false}>
                         <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="feather feather-chevron-left"><polyline points="15 18 9 12 15 6"></polyline></svg>
                     </button>
