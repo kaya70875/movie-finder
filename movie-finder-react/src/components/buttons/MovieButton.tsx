d8a58295d6f6bcd25043e0fd96426451e913be49
kaya70875 <shortsforturkey@gmail.com>
2024-10-19 01:28:54 +0300

    use toaster

--- movie-finder-react/src/components/buttons/MovieButton.tsx
+++ movie-finder-react/src/components/buttons/MovieButton.tsx
@@ -1,3 +1,4 @@
+import { toast } from "sonner";
 import { useWatchList } from "../../context/WatchListContext";
 
 interface MovieButtonProps {
@@ -17,18 +18,20 @@ export default function MovieButton({ id, button, buttonType } : MovieButtonProp
 
     if (isInWatchList) {
       deleteMovie(id);
+      toast.success('Movie removed from watchlist.');
     } else {
       addMovie(id);
+      toast.success('Movie successfully added to your watchlist.');
     }
   };
 
   const defaultBigButtonLabel = isInWatchList ? 'Remove from Watch List' : 'Add to Watch List';
   const defaultEllipseButtonSVG = isInWatchList ? (
-    <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="#F8312F" stroke="#F8312F" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="18" viewBox="0 0 24 24" fill="#F8312F" stroke="#F8312F" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
       <path d="M19 21l-7-5-7 5V5a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2z"></path>
     </svg>
   ) : (
-    <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="white" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="18" viewBox="0 0 24 24" fill="white" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
       <path d="M19 21l-7-5-7 5V5a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2z"></path>
     </svg>
   );
